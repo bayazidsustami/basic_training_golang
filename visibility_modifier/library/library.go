ac3152ea9fa85158aea6f3a972f000dc9bfeceb9
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-02 13:47:09 +0800

    accesing struct from package library

--- visibility_modifier/library/library.go
+++ visibility_modifier/library/library.go
@@ -1,1 +1,19 @@
 package library
+
+import "fmt"
+
+//access level public or exported using upper case
+func SayHello(name string) {
+	fmt.Println("hello")
+	introduce(name)
+}
+
+//access level private unexported
+func introduce(name string) {
+	fmt.Println("my name :", name)
+}
+
+type Student struct {
+	Name  string
+	Grade int
+}
--- visibility_modifier/main.go
+++ visibility_modifier/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"visibility-modifier/library"
+)
+
+func main() {
+	library.SayHello("jhon")
+
+	var s1 = library.Student{Name: "ethan", Grade: 22}
+	fmt.Println("name :", s1.Name)
+	fmt.Println("grade :", s1.Grade)
+}
