25e7e311538f44c13420e68a3f356d20166b4768
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-14 20:53:26 +0800

    create template html

--- web_servers/main.go
+++ web_servers/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	})
 
 	http.HandleFunc("/index", index)
+	http.HandleFunc("/greeting", showGreetingWithName)
 
 	fmt.Println("starting web server as http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
@@ -18,4 +20,19 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Apa Kabar")
+}
+
+func showGreetingWithName(w http.ResponseWriter, r *http.Request) {
+	var data = map[string]string{
+		"Name":    "bay bay",
+		"Message": "hello all",
+	}
+
+	var t, err = template.ParseFiles("template.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	t.Execute(w, data)
 }
