93340cfbe4c51bf20dd63442c3daa97b13670efe
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-15 22:48:56 +0800

    agregat data

--- nosql/main.go
+++ nosql/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,6 +23,7 @@ func main() {
 	//update()
 	delete()
 	find()
+	aggragateData()
 }
 
 func connect() (*mongo.Database, error) {
@@ -118,4 +120,49 @@ func delete() {
 	}
 
 	fmt.Println("remove success")
+}
+
+func aggragateData() {
+	db, err := connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	pipeline := make([]bson.M, 0)
+	err = bson.UnmarshalExtJSON([]byte(strings.TrimSpace(`
+		[
+			{ "$group": {
+				"_id": null,
+				"Total": { "$sum": 1 }
+			} },
+			{ "$project": {
+				"Total": 1,
+				"_id": 0
+			} }
+		]
+		`)), true, &pipeline)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	csr, err := db.Collection("student").Aggregate(ctx, pipeline)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer csr.Close(ctx)
+
+	result := make([]bson.M, 0)
+	for csr.Next(ctx) {
+		var row bson.M
+		err := csr.Decode(&row)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		result = append(result, row)
+	}
+
+	if len(result) > 0 {
+		fmt.Println("Total :", result[0]["Total"])
+	}
+
 }
