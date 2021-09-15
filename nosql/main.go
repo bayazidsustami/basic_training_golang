fcd79ccd01d5e50603402816df15fc55475be4c9
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-15 21:57:49 +0800

    create connection

--- nosql/main.go
+++ nosql/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var ctx = context.Background()
+
+type student struct {
+	Name  string `bson:"name"`
+	Grade int    `bson:"Grade"`
+}
+
 func main() {
 
+}
+
+func connect() (*mongo.Database, error) {
+	clientOption := options.Client()
+	clientOption.ApplyURI("mongodb://127.0.0.1:27017")
+
+	client, err := mongo.NewClient(clientOption)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Database("belajar_golang"), nil
 }
