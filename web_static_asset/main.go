9bdfc88955c52418179c17dfdcf6b26c1a668992
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-18 22:12:59 +0800

    static assets

---
+++ web_static_asset/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+	fmt.Println("server started at localhost :900")
+	http.ListenAndServe(":9000", nil)
+}
