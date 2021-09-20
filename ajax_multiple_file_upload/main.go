1bf11507267cef297edfeb6a6f530c8ba22bf9aa
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-20 23:57:23 +0800

    create project multiple upload

--- ajax_multiple_file_upload/main.go
+++ ajax_multiple_file_upload/main.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/upload", handlerUpload)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func handlerIndex(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("view.html"))
+	if err := tmpl.Execute(rw, nil); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handlerUpload(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		http.Error(rw, "only accept POST method", http.StatusBadRequest)
+		return
+	}
+
+	basePath, _ := os.Getwd()
+	reader, err := r.MultipartReader()
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+
+		fileLocation := filepath.Join(basePath, "files", part.FileName())
+		dst, err := os.Create(fileLocation)
+		if dst != nil {
+			defer dst.Close()
+		}
+
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if _, err := io.Copy(dst, part); err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	rw.Write([]byte("all files uploaded"))
 }
