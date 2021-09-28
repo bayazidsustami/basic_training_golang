d22fb7fc3ac38dfbdad1729cad5968c4f225e54e
bayazid_sustami <bayazidsustamy@gmail.com>
2021-09-28 21:13:13 +0800

    request validation

--- echo_framework/request_payload_validation.go
+++ echo_framework/request_payload_validation.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+	Age   int    `json:"age" validate:"gte=0,lte=80"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+func main() {
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	e.POST("/users", func(ctx echo.Context) error {
+		u := new(User)
+		if err := ctx.Bind(u); err != nil {
+			return err
+		}
+		if err := ctx.Validate(u); err != nil {
+			return err
+		}
+
+		return ctx.JSON(http.StatusOK, true)
+	})
+
+	e.Logger.Fatal(e.Start(":9000"))
 }
