b48c79c868037aaccbc156d1813fee8e5ed14177
bayazid_sustami <bayazidsustamy@gmail.com>
2021-10-12 20:23:00 +0800

    vendoring library

--- secure_cookie/go.mod
+++ secure_cookie/go.mod
@@ -3,8 +3,10 @@ module securecookie
 go 1.16
 
 require (
+	github.com/gorilla/securecookie v1.1.1
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/novalagung/gubrak/v2 v2.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211011170408-caeb26a5c8c0 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
--- secure_cookie/main.go
+++ secure_cookie/main.go
@@ -1,10 +1,40 @@
 package main
 
-import echo "github.com/labstack/echo/v4"
+import (
+	"net/http"
+	"time"
+
+	"github.com/gorilla/securecookie"
+	echo "github.com/labstack/echo/v4"
+)
+
+type M map[string]interface{}
+
+var sc = securecookie.New([]byte("very-secret"), []byte("a-lot-secret-yay"))
 
 func main() {
 	e := echo.New()
 
 	e.Logger.Fatal(e.Start(":9000"))
 
+}
+
+func setCookie(ctx echo.Context, name string, data M) error {
+	encoded, err := sc.Encode(name, data)
+	if err != nil {
+		return err
+	}
+
+	cookie := &http.Cookie{
+		Name:     name,
+		Value:    encoded,
+		Path:     "/",
+		Secure:   false,
+		HttpOnly: true,
+		Expires:  time.Now().Add(1 * time.Hour),
+	}
+
+	http.SetCookie(ctx.Response(), cookie)
+
+	return nil
 }
