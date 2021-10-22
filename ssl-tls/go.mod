0a588b2d0ea582ce8222aed531d7caac99c0610b
bayazid_sustami <bayazidsustamy@gmail.com>
2021-10-22 20:42:58 +0800

    http ssl-tls self signed

---
+++ ssl-tls/go.mod
@@ -0,0 +1,3 @@
+module ssl-tls
+
+go 1.16
---
+++ ssl-tls/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func StartNotTLSServer() {
+	mux := new(http.ServeMux)
+	mux.Handle("/", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("redirecting to https://localhost")
+		http.Redirect(writer, request, "https://localhost", http.StatusTemporaryRedirect)
+	}))
+
+	log.Fatal(http.ListenAndServe(":80", mux))
+}
+
+func main() {
+	go StartNotTLSServer()
+
+	mux := new(http.ServeMux)
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello World"))
+	})
+
+	log.Println("Server started at : 443")
+
+	/**
+	if you're using serveTLS it's automatically enabled http/2
+	*/
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", mux)
+	if err != nil {
+		panic(err)
+	}
+}
