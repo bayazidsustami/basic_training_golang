44e79587c522884c5367703850661fdf20312f7b
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-11 21:47:10 +0800

    feat : type data string

---
+++ golang-ekokurniadi/belajar-golang-string/go.mod
@@ -0,0 +1,3 @@
+module belajar-golang-string
+
+go 1.21.3
---
+++ golang-ekokurniadi/belajar-golang-string/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Bay")
+	fmt.Println("Bayazid")
+	fmt.Println("Bayazid Sustami")
+
+	fmt.Println(len("Bay"))   //ambil panjang
+	fmt.Println("Bayazid"[0]) //ambil karakter pertama dalam bentuk byte
+	fmt.Println("Bayazid Sustami"[1])
+}
