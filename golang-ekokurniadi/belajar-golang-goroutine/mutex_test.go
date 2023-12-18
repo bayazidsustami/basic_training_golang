33e09cf0889e309d29ed1740a54ba2176590321f
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-19 00:02:28 +0800

    feat : deadlock problem

--- golang-ekokurniadi/belajar-golang-goroutine/mutex_test.go
+++ golang-ekokurniadi/belajar-golang-goroutine/mutex_test.go
@@ -57,3 +57,59 @@ func TestReadWriteMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("total :", account.GetBalance())
 }
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("lock 1", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("lock 2", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := UserBalance{
+		Name:    "bay",
+		Balance: 1000000,
+	}
+
+	user2 := UserBalance{
+		Name:    "yazid",
+		Balance: 1000000,
+	}
+
+	go Transfer(&user1, &user2, 100000)
+	go Transfer(&user2, &user1, 200000)
+
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("user 1:", user1.Name, " balance", user1.Balance)
+	fmt.Println("user 2:", user2.Name, " balance", user2.Balance)
+
+}
