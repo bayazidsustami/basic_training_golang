a28ef3ad8fd76862c7eb1807e5d0e26d2bd0bf53
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-18 23:07:22 +0800

    feat : select channel

--- golang-ekokurniadi/belajar-golang-goroutine/channel_test.go
+++ golang-ekokurniadi/belajar-golang-goroutine/channel_test.go
@@ -98,3 +98,32 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeRespose(channel1)
+	go GiveMeRespose(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("menunggu data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
