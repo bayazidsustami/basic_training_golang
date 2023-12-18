7d9812d8f3d5db19a8eea5b505f2cd733ce9c80b
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-18 23:15:56 +0800

    feat : race condition simulation

---
+++ golang-ekokurniadi/belajar-golang-goroutine/race_condition_test.go
@@ -0,0 +1,20 @@
+package belajargolanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRaceCondition(t *testing.T) {
+	var x = 0
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("counter :", x)
+}
