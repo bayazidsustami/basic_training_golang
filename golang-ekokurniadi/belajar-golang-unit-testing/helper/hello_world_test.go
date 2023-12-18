7eee326f601e7317eae6d46bbc7c7781312facb0
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-18 11:30:33 +0800

    feat : test assertion

--- golang-ekokurniadi/belajar-golang-unit-testing/helper/hello_world_test.go
+++ golang-ekokurniadi/belajar-golang-unit-testing/helper/hello_world_test.go
@@ -1,6 +1,11 @@
 package helper
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestHelloWorldSuccess(t *testing.T) {
 	result := HelloWorld("bay")
@@ -24,4 +29,14 @@ func TestHelloWorldWithString(t *testing.T) {
 		//t.FailNow() // langsung  menghentikan eksekusi code
 		t.Fatal("harusnya bayazid sustamis") // memanggil t.FailNow() dengan message
 	}
+}
+
+func TestMultiplicationAssertion(t *testing.T) {
+	result := Multiplication(1, 2)
+	assert.Equal(t, 2, result) // mirip t.Fail()
+}
+
+func TestMultiplicationRequire(t *testing.T) {
+	result := Multiplication(1, 2)
+	require.Equal(t, 2, result) // mirip t.FailNow()
 }
