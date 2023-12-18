d533405c89631fa486e0b1fb6d40c51d50802805
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-18 22:54:29 +0800

    feat : range channel

--- golang-ekokurniadi/belajar-golang-goroutine/channel_test.go
+++ golang-ekokurniadi/belajar-golang-goroutine/channel_test.go
@@ -2,6 +2,7 @@ package belajargolanggoroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -78,5 +79,22 @@ func TestBufferedChannel(t *testing.T) {
 	}()
 
 	time.Sleep(2 * time.Second)
+	fmt.Println("selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima", data)
+	}
+
 	fmt.Println("selesai")
 }
