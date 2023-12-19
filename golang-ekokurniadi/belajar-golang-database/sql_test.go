a5ceda53d3958ae06fdbe603a574734e9c898f73
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-19 17:16:56 +0800

    feat : query sql

--- golang-ekokurniadi/belajar-golang-database/sql_test.go
+++ golang-ekokurniadi/belajar-golang-database/sql_test.go
@@ -20,3 +20,28 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("success insert new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	context := context.Background()
+
+	querySelect := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(context, querySelect)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id", id)
+		fmt.Println("name", name)
+	}
+}
