d2e804c9aed2ad24ea9cd5c1dd4bf4fb1c05fca3
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-20 11:20:26 +0800

    feat : query sql with complex data

--- golang-ekokurniadi/belajar-golang-database/sql_test.go
+++ golang-ekokurniadi/belajar-golang-database/sql_test.go
@@ -2,8 +2,10 @@ package belajargolangdatabase
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -43,5 +45,36 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("id", id)
 		fmt.Println("name", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	context := context.Background()
+
+	querySelect := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(context, querySelect)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString // support nullable type
+		var balance int32
+		var rating float64
+		var birthDate sql.NullTime // support nullable type
+		var createdAt time.Time
+		var married bool
+
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id :", id, " Name:", name, " Email:", email, " Balance", balance, " Rating:", rating, " Birth Date:", birthDate, " Married:", married, " Created At:", createdAt)
 	}
 }
