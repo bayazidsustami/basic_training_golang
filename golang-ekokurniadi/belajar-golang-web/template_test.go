1a093d96cd27a3142a0e34eb3063f56e7a861d28
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-21 21:06:28 +0800

    feat : simple html file

--- golang-ekokurniadi/belajar-golang-web/template_test.go
+++ golang-ekokurniadi/belajar-golang-web/template_test.go
@@ -16,11 +16,34 @@ func SimpleHTML(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "SIMPLE", "Hello from simple html template")
 }
 
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./templates/simple.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(w, "simple.gohtml", "hello from template")
+}
+
 func TestSimpleHTML(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
 
 	SimpleHTML(recorder, request)
+
+	body, err := io.ReadAll(recorder.Result().Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTMLFile(recorder, request)
 
 	body, err := io.ReadAll(recorder.Result().Body)
 	if err != nil {
