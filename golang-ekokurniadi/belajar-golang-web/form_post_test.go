8d15aa0f79d8dca7a7e250d6153fd4bd0315ce93
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-21 16:47:29 +0800

    feat : form post

---
+++ golang-ekokurniadi/belajar-golang-web/form_post_test.go
@@ -0,0 +1,37 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := r.PostForm.Get("first_name")
+	lastName := r.PostForm.Get("last_name")
+
+	fmt.Fprintf(w, "Hello %s %s", firstName, lastName)
+}
+
+func TestHttpFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=bay&last_name=bays")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
