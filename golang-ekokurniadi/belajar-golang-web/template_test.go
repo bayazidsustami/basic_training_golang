958341c8be295ef1539b27eeb1be05518932402d
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-21 20:56:37 +0800

    feat : simple http template

---
+++ golang-ekokurniadi/belajar-golang-web/template_test.go
@@ -0,0 +1,31 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHTML(w http.ResponseWriter, r *http.Request) {
+	templateText := `<html><body>{{.}}</body></html>`
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+
+	t.ExecuteTemplate(w, "SIMPLE", "Hello from simple html template")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTML(recorder, request)
+
+	body, err := io.ReadAll(recorder.Result().Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
