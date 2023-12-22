7097eee018868b240c001b7684cc85ace6a93ba9
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-22 17:29:42 +0800

    feat : router param

--- golang-ekokurniadi/belajar-golang-http-router/router_test.go
+++ golang-ekokurniadi/belajar-golang-http-router/router_test.go
@@ -26,3 +26,20 @@ func TestRouter(t *testing.T) {
 	bytes, _ := io.ReadAll(response.Body)
 	assert.Equal(t, "Hello World", string(bytes))
 }
+
+func TestRouterParams(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		text := "Product " + p.ByName("id")
+		fmt.Fprint(w, text)
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/products/1", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Product 1", string(bytes))
+}
