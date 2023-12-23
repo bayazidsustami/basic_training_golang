a24e4625d7eba65012d8ed35b295a697ad06eec1
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-23 10:39:31 +0800

    feat : router not found handler

---
+++ golang-ekokurniadi/belajar-golang-http-router/router_not_found_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFoundHandler(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "not found")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "not found", string(bytes))
+}
