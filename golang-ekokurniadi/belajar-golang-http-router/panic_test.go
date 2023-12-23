e994eaee3f91cba5a5f31de690694b7f368992b3
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-23 10:35:16 +0800

    feat : httprouter panic handler

---
+++ golang-ekokurniadi/belajar-golang-http-router/panic_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanicHandler(t *testing.T) {
+	router := httprouter.New()
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, err interface{}) {
+		fmt.Fprint(w, "Panic : ", err)
+	}
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		panic("ups")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Panic : ups", string(bytes))
+}
