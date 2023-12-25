9e440e3ecef37f1d14d15a3bd6a7de728bcb0698
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-25 14:05:15 +0800

    feat : provide controller

---
+++ golang-ekokurniadi/belajar-golang-restful/app/database.go
@@ -0,0 +1,19 @@
+package app
+
+import (
+	"belajar-golang-restful/utils"
+	"database/sql"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_restful_api")
+	utils.PanicErr(err)
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
---
+++ golang-ekokurniadi/belajar-golang-restful/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"belajar-golang-restful/app"
+	"belajar-golang-restful/controller"
+	"belajar-golang-restful/repository"
+	"belajar-golang-restful/service"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	validate := validator.New()
+	db := app.NewDB()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	controller := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", controller.FindAll)
+	router.GET("/api/categories/:categoryId", controller.FindById)
+	router.POST("/api/categories", controller.Create)
+	router.PUT("/api/categories/:categoryId", controller.Update)
+	router.DELETE("/api/categories/:categoryId", controller.Delete)
+
+}
