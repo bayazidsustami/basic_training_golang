ecc73fd8e2de0abae68df9e37ec9c2eb5f6f7c26
bayazidsustami <bayazidsustamy@gmail.com>
2023-12-25 15:50:29 +0800

    feat : error validation

--- golang-ekokurniadi/belajar-golang-restful/exception/error_handler.go
+++ golang-ekokurniadi/belajar-golang-restful/exception/error_handler.go
@@ -4,13 +4,39 @@ import (
 	"belajar-golang-restful/model/web"
 	"belajar-golang-restful/utils"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err any) {
 	if notFoundError(writer, request, err) {
 		return
 	}
+
+	if validationError(writer, request, err) {
+		return
+	}
+
 	internalServerError(writer, request, err)
+}
+
+func validationError(writer http.ResponseWriter, request *http.Request, err any) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+			Data:   exception.Error(),
+		}
+
+		utils.WriteToResponseBody(writer, webResponse)
+		return true
+	} else {
+		return false
+	}
 }
 
 func notFoundError(writer http.ResponseWriter, request *http.Request, err any) bool {
