34dd2aa2820f9abc1745e4bcd9d00d738bde7f58
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-05 00:46:56 +0800

    feat : create di

---
+++ golang-ekokurniadi/belajar-golang-restful/injector.go
@@ -0,0 +1,41 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"belajar-golang-restful/app"
+	"belajar-golang-restful/controller"
+	"belajar-golang-restful/middleware"
+	"belajar-golang-restful/repository"
+	"belajar-golang-restful/service"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
+)
+
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	service.NewCategoryService,
+	controller.NewCategoryController,
+)
+
+var validatorSet = wire.NewSet(
+	validator.New,
+	wire.Value([]validator.Option{}),
+)
+
+func InitializedServer() *http.Server {
+	wire.Build(
+		app.NewDB,
+		validatorSet,
+		categorySet,
+		app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+	return nil
+}
--- golang-ekokurniadi/belajar-golang-restful/main.go
+++ golang-ekokurniadi/belajar-golang-restful/main.go
@@ -1,34 +1,23 @@
 package main
 
 import (
-	"belajar-golang-restful/app"
-	"belajar-golang-restful/controller"
 	"belajar-golang-restful/middleware"
-	"belajar-golang-restful/repository"
-	"belajar-golang-restful/service"
 	"belajar-golang-restful/utils"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/go-playground/validator/v10"
 )
 
-func main() {
-	validate := validator.New()
-	db := app.NewDB()
-
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	controller := controller.NewCategoryController(categoryService)
-
-	router := app.NewRouter(controller)
-
-	server := http.Server{
+func NewServer(autMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: autMiddleware,
 	}
+}
 
+func main() {
+
+	server := InitializedServer()
 	err := server.ListenAndServe()
 	utils.PanicErr(err)
 
