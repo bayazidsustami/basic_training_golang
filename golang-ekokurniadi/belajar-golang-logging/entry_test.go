20978ca6105a4bee0d4f2e3aa22e00a5a53c8863
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-08 21:42:44 +0800

    feat : logger newEntry()

---
+++ golang-ekokurniadi/belajar-golang-logging/entry_test.go
@@ -0,0 +1,15 @@
+package belajar_golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestEntry(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	entry := logrus.NewEntry(logger)
+	entry.WithField("username", "bayazid").Info("Hello World")
+	entry.Log(logrus.WarnLevel, "warning")
+}
