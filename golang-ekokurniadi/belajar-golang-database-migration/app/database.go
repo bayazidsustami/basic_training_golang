51389ca80939457162766606bc50519bd6d61418
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-08 23:05:29 +0800

    feat : run migration

--- golang-ekokurniadi/belajar-golang-database-migration/app/database.go
+++ golang-ekokurniadi/belajar-golang-database-migration/app/database.go
@@ -7,13 +7,16 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_restful_api")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_database_migration")
 	utils.PanicErr(err)
 
 	db.SetMaxIdleConns(10)
 	db.SetMaxOpenConns(100)
 	db.SetConnMaxIdleTime(5 * time.Minute)
 	db.SetConnMaxLifetime(60 * time.Minute)
+
+	//migration command
+	//migrate -database "mysql://root:@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up
 
 	return db
 }
