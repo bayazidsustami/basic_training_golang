b0516c06ca695ffba7ef52ba8fcbbb218ffd03f4
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-08 20:41:21 +0800

    feat : create logger project

---
+++ golang-ekokurniadi/belajar-golang-logging/logger_test.go
@@ -0,0 +1,12 @@
+package belajar_golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Println("Hello Logger")
+}
