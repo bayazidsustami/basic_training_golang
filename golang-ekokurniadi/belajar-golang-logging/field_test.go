a9395b9e4e785d190bd32761206e7e642c431059
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-08 21:33:22 +0800

    feat : log fields

---
+++ golang-ekokurniadi/belajar-golang-logging/field_test.go
@@ -0,0 +1,26 @@
+package belajar_golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestField(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.WithField("username", "bayazid").Info("Hello World")
+	logger.WithField("username", "bay").
+		WithField("name", "bay bay").
+		Info("Hello World")
+}
+
+func TestFields(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	logger.WithFields(logrus.Fields{
+		"username": "bay",
+		"name":     "bay bay",
+	}).Info("Hello World")
+}
