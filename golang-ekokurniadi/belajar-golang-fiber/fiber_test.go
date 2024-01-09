a6677472693d3e42b66e0962543fc1cbb1c22513
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-09 16:59:51 +0800

    feat : fiber context

--- golang-ekokurniadi/belajar-golang-fiber/fiber_test.go
+++ golang-ekokurniadi/belajar-golang-fiber/fiber_test.go
@@ -26,3 +26,41 @@ func TestRoutingHelloWorld(t *testing.T) {
 	assert.Equal(t, "Hello World!", string(byte))
 
 }
+
+func TestFiberCtx(t *testing.T) {
+	app := fiber.New()
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Query("name", "Guest")
+
+		return c.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/hello?name=bay", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	byte, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello bay", string(byte))
+
+}
+
+func TestFiberCtxDefaultQuery(t *testing.T) {
+	app := fiber.New()
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Query("name", "Guest")
+
+		return c.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	byte, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(byte))
+
+}
