fc53524e46c1b0a900e3a1ebae8ebb9e178d9e37
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-09 23:31:01 +0800

    feat : enable prefork

--- golang-ekokurniadi/belajar-golang-fiber/main.go
+++ golang-ekokurniadi/belajar-golang-fiber/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,11 +12,18 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
+		Prefork:      true,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
 	})
+
+	if fiber.IsChild() {
+		fmt.Println("this is chiled")
+	} else {
+		fmt.Println("this is parent")
+	}
 
 	err := app.Listen(":3000")
 	if err != nil {
