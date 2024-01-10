b6004a3da23b31b151cc2a6984eab92359f381cf
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-10 15:09:17 +0800

    feat : multiple type parameter

--- golang-ekokurniadi/belajar-golang-generic/type_parameter_test.go
+++ golang-ekokurniadi/belajar-golang-generic/type_parameter_test.go
@@ -12,10 +12,20 @@ func Length[T any](param T) T {
 	return param
 }
 
+func MultipleParameter[T1 any, T2 any](param1 T1, param2 T2) {
+	fmt.Println(param1)
+	fmt.Println(param2)
+}
+
 func TestSampleGeneric(t *testing.T) {
 	var result string = Length[string]("Bayazid")
 	assert.Equal(t, "Bayazid", result)
 
 	var resultNumber int = Length[int](100)
 	assert.Equal(t, 100, resultNumber)
+}
+
+func TestMultipleParameter(t *testing.T) {
+	MultipleParameter[string, int]("test string", 100)
+	MultipleParameter[float32, bool](23.4, true)
 }
