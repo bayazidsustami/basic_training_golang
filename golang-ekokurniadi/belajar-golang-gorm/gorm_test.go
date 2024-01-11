46222789f41943e117f1e2e797d2ff5c3da5e888
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-11 23:25:34 +0800

    feat : one to one relation

--- golang-ekokurniadi/belajar-golang-gorm/gorm_test.go
+++ golang-ekokurniadi/belajar-golang-gorm/gorm_test.go
@@ -450,3 +450,32 @@ func TestLock(t *testing.T) {
 
 	assert.Nil(t, err)
 }
+
+func TestCreateWallet(t *testing.T) {
+	wallet := Wallet{
+		ID:      "1",
+		UserId:  "1",
+		Balance: 1000000,
+	}
+
+	err := db.Create(&wallet).Error
+	assert.Nil(t, err)
+}
+
+func TestRetrieveRelation(t *testing.T) {
+	var user User
+	err := db.Model(&User{}).Preload("Wallet").Take(&user).Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, "1", user.ID)
+	assert.Equal(t, "1", user.Wallet.ID)
+}
+
+func TestRetrieveRelationJoins(t *testing.T) {
+	var user User
+	err := db.Model(&User{}).Joins("Wallet").Take(&user, "users.id = ?", "1").Error
+	assert.Nil(t, err)
+
+	assert.Equal(t, "1", user.ID)
+	assert.Equal(t, "1", user.Wallet.ID)
+}
