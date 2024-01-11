3e1e996eeaa5b09a5a87006d76c1d0072aa0bd93
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-11 10:30:56 +0800

    feat : create db connection

---
+++ golang-ekokurniadi/belajar-golang-gorm/database.go
@@ -0,0 +1,16 @@
+package belajargolanggorm
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func OpenConnection() *gorm.DB {
+	dialect := mysql.Open("root:@tcp(127.0.0.1:3306)/belajar_golang_gorm?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(dialect, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
