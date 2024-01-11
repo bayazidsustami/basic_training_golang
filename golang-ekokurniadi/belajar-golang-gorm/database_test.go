b3b4731f3de1e7c0ea29f8f99e39967e8ce02e9e
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-11 10:42:32 +0800

    feat : execute raw sql

--- golang-ekokurniadi/belajar-golang-gorm/database_test.go
+++ golang-ekokurniadi/belajar-golang-gorm/database_test.go
@@ -11,3 +11,17 @@ var db = OpenConnection()
 func TestOpenConnection(t *testing.T) {
 	assert.NotNil(t, db)
 }
+
+func TestExecuteSql(t *testing.T) {
+	err := db.Exec("insert into sample(id, name) values(?,?)", "1", "sambo").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "2", "lacukka ulu").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "3", "ambo nai").Error
+	assert.Nil(t, err)
+
+	err = db.Exec("insert into sample(id, name) values(?,?)", "4", "beddu").Error
+	assert.Nil(t, err)
+}
