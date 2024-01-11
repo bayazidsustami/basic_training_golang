532b400a33410aa1148f6df71cee24522857cb5b
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-11 22:55:44 +0800

    feat : struct gorm model

--- golang-ekokurniadi/belajar-golang-gorm/todo.go
+++ golang-ekokurniadi/belajar-golang-gorm/todo.go
@@ -1,19 +1,14 @@
 package belajargolanggorm
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Todo struct {
-	ID          string    `gorm:"primary_key;column:id;autoIncrement"`
-	UserId      string    `gorm:"column:user_id"`
-	Title       string    `gorm:"column:title"`
-	Description string    `gorm:"column:description"`
-	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
-	DeletedAt   gorm.DeletedAt
+	gorm.Model
+	UserId      string `gorm:"column:user_id"`
+	Title       string `gorm:"column:title"`
+	Description string `gorm:"column:description"`
 }
 
 func (t *Todo) TableName() string {
