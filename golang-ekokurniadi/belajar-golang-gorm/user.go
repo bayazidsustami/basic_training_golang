fc340976a6a6592e4c07343c15ac762703c2b5e6
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-11 11:27:31 +0800

    feat : field permission

--- golang-ekokurniadi/belajar-golang-gorm/user.go
+++ golang-ekokurniadi/belajar-golang-gorm/user.go
@@ -3,11 +3,12 @@ package belajargolanggorm
 import "time"
 
 type User struct {
-	ID        string    `gorm:"primary_key;column:id"`
-	Password  string    `gorm:"column:password"`
-	Name      string    `gorm:"column:name"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID          string    `gorm:"primary_key;column:id;<-:create"`
+	Password    string    `gorm:"column:password"`
+	Name        string    `gorm:"column:name"`
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-create"`
+	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Information string    `gorm:"-"` //ignored
 }
 
 // custom table name if not match with gorm convention
