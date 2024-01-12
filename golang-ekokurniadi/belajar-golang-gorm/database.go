dcbc452b166a8c84d8cb700086315a07dcbc799c
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-12 11:34:43 +0800

    feat : gorm connection pool

--- golang-ekokurniadi/belajar-golang-gorm/database.go
+++ golang-ekokurniadi/belajar-golang-gorm/database.go
@@ -1,6 +1,8 @@
 package belajargolanggorm
 
 import (
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -14,6 +16,16 @@ func OpenConnection() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+	sqlDB.SetConnMaxIdleTime(5 * time.Minute)
 
 	return db
 }
