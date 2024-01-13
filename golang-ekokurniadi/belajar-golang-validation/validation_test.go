424d50e155f0930e0bce7a1d835311fe02c33b38
bayazidsustami <bayazidsustamy@gmail.com>
2024-01-13 14:40:58 +0800

    feat : create validation test

---
+++ golang-ekokurniadi/belajar-golang-validation/validation_test.go
@@ -0,0 +1,42 @@
+package belajargolangvalidation
+
+import (
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidation(t *testing.T) {
+	validate := validator.New()
+	if validate == nil {
+		t.Error("validate is nil")
+	}
+}
+
+func TestValidationVariable(t *testing.T) {
+	validate := validator.New()
+	user := ""
+
+	err := validate.Var(user, "required")
+
+	assert.NotNil(t, err)
+}
+
+func TestValidationTwoVariable(t *testing.T) {
+	validate := validator.New()
+	password := "password"
+	confirmPassword := "salah"
+
+	err := validate.VarWithValue(password, confirmPassword, "eqfield")
+
+	assert.NotNil(t, err)
+}
+
+func TestMultipleTag(t *testing.T) {
+	validate := validator.New()
+	user := "12345"
+
+	err := validate.Var(user, "required,numeric")
+	assert.Nil(t, err)
+}
